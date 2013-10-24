eeaf924ec754fb521dd32ed79245a46ee499e424
Øistein Sørensen <sorenso@gmail.com>
2013-10-24 15:24:57 +0200

    Moved init function into plugins.

--- plugins/boilerplate.js
+++ plugins/boilerplate.js
@@ -1,9 +1,8 @@
-var Boilerplate = function () {
+var Boilerplate = function (options) {
     var that;
     var version = '1.0.0';
     var counter = {};
     var logger;
-
 
     function init(opt){
         var Logger = require('bunyan');
@@ -20,7 +19,6 @@ var Boilerplate = function () {
             serializers: Logger.stdSerializers
         });
     }
-
 
     function execute(message) {
         var json = message;
@@ -45,6 +43,9 @@ var Boilerplate = function () {
         init: init,
         execute: execute
     };
+
+    // Call init when module is constructed.
+    init(options);
 
     // Return object to make functions accessible.
     return that;
--- plugins/file_logger.js
+++ plugins/file_logger.js
@@ -1,9 +1,8 @@
-var Filelogger = function () {
+var Filelogger = function (options) {
     var that;
     var version = '1.0.0';
     var counter = {};
     var logger;
-
 
     function init(opt){
         var Logger = require('bunyan');
@@ -20,7 +19,6 @@ var Filelogger = function () {
             serializers: Logger.stdSerializers
         });
     }
-
 
     function execute(message) {
         var json = message;
@@ -39,6 +37,9 @@ var Filelogger = function () {
         init: init,
         execute: execute
     };
+
+    // Call init when module is constructed.
+    init(options);
 
     // Return object to make functions accessible.
     return that;
