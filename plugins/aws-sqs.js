a68d1794f86fb99601c181e7897b4f9354b708fe
Magne Thyrhaug <magne@thyrhaug.net>
2013-10-25 13:34:19 +0200

    Naive sqs batch send operation

--- plugins/aws-sqs.js
+++ plugins/aws-sqs.js
@@ -1,9 +1,11 @@
 var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
 
 var aws_sqs = function (options) {
     var version = '1.0.0';
     var sqs;
     var name = "aws-sqs";
+    var internal_queue = [];
 
     function init(){
         AWS.config.loadFromPath(options.credentials_file);
@@ -11,12 +13,30 @@ var aws_sqs = function (options) {
     }
 
     function execute(message, callback) {
-        sqs.sendMessage({
-            QueueUrl: options.queue_url,
-            MessageBody: JSON.stringify(message)
-        }, function aws_sqs_callback(err, data) {
-            callback();
-        });
+        if(options.batch_size > 1) {
+            // too naïve.
+            internal_queue.push({
+                Id: uuid.v4(),
+                MessageBody: message
+            });
+
+            if(internal_queue.length > 9) {
+                sqs.sendMessageBatch({
+                    QueueUrl: options.queue_url,
+                    Entries: internal_queue
+                }, function(err, data) {
+                    callback();
+                });
+                internal_queue = [];
+            }
+        } else {
+            sqs.sendMessage({
+                QueueUrl: options.queue_url,
+                MessageBody: JSON.stringify(message)
+            }, function aws_sqs_callback(err, data) {
+                callback();
+            });
+        }
     }
 
     init();
