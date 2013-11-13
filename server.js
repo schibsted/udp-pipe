81c7a104c7d13f1830da9319aaea10a87a8b58f7
Øistein Sørensen <sorenso@gmail.com>
2013-11-13 15:44:22 +0100

    re #19 #20 Added timestamp and config reload.

--- server.js
+++ server.js
@@ -12,16 +12,19 @@ var util = require('./lib/util.js');
 // TODO: Merge config file and opt with config file as primary OR should we do this only for the master process?
 var argv = util.process_args();
 // TODO: Merge argv and opt with arg as primary
-
-var opt = require(argv.c || './config/config.js');
+var config_file = argv.c || __dirname + '/config/config.js';
+var opt = require(config_file);
 
 var Logserver = require('./lib/logserver.js');
 var logserver = new Logserver();
 var stats_server = require('./lib/stats_server.js');
 
 process.on('SIGHUP', function () {
+    console.log(util.iso_date() + ':');
     console.log('Got SIGHUP signal.'.red);
-    console.log('Recycling log handles.'.red);
+    console.log('Recycling log file.'.red);
+    opt = logserver.reload_module(config_file);
+    console.log('Reloading plugins.'.red);
     logserver.reload_process(opt);
 });
 
