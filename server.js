325a093e0de200f9be7bd6681a63006dffe9e254
Øistein Sørensen <sorenso@gmail.com>
2014-11-07 13:59:05 +0100

    Refactored.

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-var colors = require('colors');
-var util = require('./lib/util.js');
+/*
+ * https://github.com/schibsted
+ *
+ * Copyright (c) 2014 Schibsted Payment
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+var colors       = require('colors'),
+    app_path      = __dirname + '/',
+    logger       = require(app_path + 'lib/logger')(),
+    util         = require(app_path + 'lib/util.js')({ logger: logger }),
+    logserver    = require(app_path + 'lib/logserver.js')({ logger: logger }),
+    stats_server = require(app_path + 'lib/stats-server.js')({ logger: logger });
 
 // TODO: Merge config file and opt with config file as primary OR should we do this only for the master process?
 var argv = util.process_args();
@@ -7,25 +19,21 @@ var argv = util.process_args();
 var config_file = argv.c || __dirname + '/config/config.js';
 var opt = require(config_file);
 
-console.log("\n\n" +
-    'SPiD UDP LogServer ' + util.iso_timestamp() +
-    "\n" +
-    '=======================================' +
-    "");
-
-var Logserver = require('./lib/logserver.js');
-var logserver = new Logserver();
-var stats_server = require('./lib/stats_server.js');
+logger.log('SPiD UDP LogServer ' + util.iso_timestamp());
 
 process.on('SIGHUP', function () {
-    console.log(util.iso_timestamp() + ':');
-    console.log('Got SIGHUP signal.'.red);
-    console.log('Recycling log file.'.red);
+    logger.log(util.iso_timestamp() + ':');
+    logger.log('Got SIGHUP signal.'.red);
+    logger.log('Recycling log file.'.red);
     opt = logserver.reload_module(config_file);
-    console.log('Reloading plugins.'.red);
+    logger.log('Reloading plugins.'.red);
     logserver.reload_process(opt);
 });
 
 var stats = stats_server.create_process_stats();
 stats_server.start(opt, __dirname, stats);
-logserver.start_udp_server(opt, stats);
+logserver.start_udp_server(opt, stats, function (address) {
+    logger.log('Up and listening for UDP on: ' + address.address + ':' + address.port);
+});
+
+
