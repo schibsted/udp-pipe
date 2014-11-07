d89b03fbe4fc8e73fd8976fa7acff904b5cc4058
Øistein Sørensen <sorenso@gmail.com>
2014-11-07 13:57:46 +0100

    Added tests and Travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,77 @@
+'use strict';
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['server.js', 'lib/*.js'],
             options: {
-
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                src: ['app/**/*.js', 'lib/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js']
             }
         },
-        mochaTest: {
-            test: {
-                options: {
-                    require: 'coffee-script'
-                },
-                src: ["test/*.coffee"]
+        watch: {
+            all: {
+                files: ['*.js', 'client/**/*.js', 'lib/**/*.js', 'test/**/*.js', 'plugins/**/*.js', 'config/*.js'],
+                tasks: ['jshint', 'buster:unit']
+            }
+        },
+        buster: {
+            unit: {
             }
         },
         nodemon: {
             dev: {
                 options: {
-                    file: 'server.js'
-                }
+                    file: './server.js',
+                    args: ['-c', './config/config-dist.js']
+                },
+                tasks: ['jshint', 'buster:unit']
             },
-            acceptance: {
+            dev_local: {
                 options: {
-                    file: 'server.js',
-                    args: ['-c', './config/config-acceptance.js']
-                }
+                    file: './server.js',
+                    args: ['-c', './config/config-local.js']
+                },
+                tasks: ['jshint', 'buster:unit']
             }
-        }/*,
-        concurrent: {
-            acceptanceTest: {
-                tasks: ['nodemon', 'mochaTest'],
-                options: {
-                    logConcurrentOutput: true
-                }
+        },
+        shell: {
+            multiple: {
+                command: [
+                    'rm -rf artifact',
+                    'mkdir -p artifact',
+                    'mv node_modules ../node_modules2',
+                    'npm install --production',
+                    'tar -zcf artifact/udplogger.tar.gz .',
+                    'rm -rf node_modules',
+                    'mv ../node_modules2 node_modules'
+                ].join('&&')
             }
-        }*/
+        }
     });
 
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-nodemon');
-    //grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-buster');
 
-    grunt.registerTask('default', ['jshint']);
-    grunt.registerTask('test', 'mochaTest');
-    grunt.registerTask('acceptance', function () {
-        grunt.config.set('mochaTest.test.src', "test_acceptance/*.coffee");
-        grunt.task.run('mochaTest');
-    });
-    grunt.registerTask('run', function() {
-        grunt.task.run('nodemon');
-    });
-    grunt.registerTask('runAcceptance', function() {
-        grunt.task.run('nodemon:acceptance');
-    })
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'buster:unit']);
+    grunt.registerTask('test', 'buster:unit');
+    grunt.registerTask('check', ['watch']);
+    grunt.registerTask('run', ['buster:unit', 'nodemon:dev']);
+    grunt.registerTask('run-local', ['buster:unit', 'nodemon:dev_local']);
+    grunt.registerTask('artifact', ['shell']);
 
-}
+};
