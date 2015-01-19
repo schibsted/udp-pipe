f6250666743c05c32f6ba9fb986307c6df42f937
Øistein Sørensen <sorenso@gmail.com>
2015-01-19 12:16:33 +0100

    Fixed logging and cleaned up a bit.

--- server.js
+++ server.js
@@ -7,33 +7,29 @@
 'use strict';
 
 var app_path      = __dirname + '/',
-    logger       = require(app_path + 'lib/logger')(),
-    util         = require(app_path + 'lib/util.js')({ logger: logger }),
-    logserver    = require(app_path + 'lib/logserver.js')({ logger: logger }),
-    stats_server = require(app_path + 'lib/stats-server.js')({ logger: logger });
-
-// TODO: Merge config file and opt with config file as primary OR should we do this only for the master process?
-var argv = util.process_args();
-// TODO: Merge argv and opt with arg as primary
-var config_file = argv.c || __dirname + '/config/config.js';
-var opt = require(config_file);
+    util          = require(app_path + 'lib/util.js')(),
+    argv          = util.process_args(),
+    config_file   = argv.c || __dirname + '/config/config.js',
+    opt           = require(config_file),
+    logger        = require(app_path + 'lib/logger')(opt),
+    logserver     = require(app_path + 'lib/logserver.js')({ logger: logger }),
+    stats_server  = require(app_path + 'lib/stats-server.js')({ logger: logger });
 
 logger.log('SPiD UDP LogServer ' + util.iso_timestamp());
 
 process.on('SIGHUP', function () {
     logger.log(util.iso_timestamp() + ':');
-    logger.log('Got SIGHUP signal.');
-    logger.log('Recycling log file.');
+    logger.log('info', 'Got SIGHUP signal.');
+    logger.log('info', 'Recycling log file.');
     opt = logserver.reload_module(config_file);
-    logger.log('Reloading plugins.');
+    logger.log('info', 'Reloading plugins.');
     logserver.reload_process(opt);
 });
 
 var stats = stats_server.create_process_stats();
 stats_server.start(opt, app_path, stats, function () {
     logserver.start_udp_server(opt, stats, function (address) {
-        logger.log('Up and listening for UDP on: ' + address.address + ':' + address.port);
-        logger.log('debug','Up and listening for UDP on: ' + address.address + ':' + address.port);
+        logger.log('info', 'Up and listening for UDP on: ' + address.address + ':' + address.port);
     });
 });
 
