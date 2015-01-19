4d25643f64188b39f9645e33c7023a49e57b667d
Øistein Sørensen <sorenso@gmail.com>
2015-01-19 12:05:33 +0100

    Added auto code coverage.

--- test/buster.js
+++ test/buster.js
@@ -5,5 +5,19 @@ config["My tests"] = {
     rootPath: "../",
     tests: [
         "test/**/*-test.js"
+    ],
+
+    // buster-istanbul setup
+    "buster-istanbul": {
+        outputDirectory: "coverage",
+        format: "lcov"
+    },
+    sources: [
+        "server.js",
+        "lib/**/*.js",
+        "plugins/**/*.js"
+    ],
+    extensions: [
+        require('buster-istanbul')
     ]
 };
